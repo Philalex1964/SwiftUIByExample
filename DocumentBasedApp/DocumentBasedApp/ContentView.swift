a6fdd889bb35df1cfd44b75cc2e2b3724dbcc2d8
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-30 15:47:15 +0200

    How to create a document-based app using FileDocument and DocumentGroup

--- DocumentBasedApp/DocumentBasedApp/ContentView.swift
+++ DocumentBasedApp/DocumentBasedApp/ContentView.swift
@@ -8,14 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+    @Binding var document: TextFile
+
+        var body: some View {
+            TextEditor(text: $document.text)
+        }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
