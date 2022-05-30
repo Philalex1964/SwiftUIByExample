aa9eef23758ea37cc7d1bc050ae02af8be668904
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-30 16:13:24 +0200

    set up for refactoring

---
+++ DocBasedApp/DocBasedApp/DocBasedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocBasedAppApp.swift
+//  DocBasedApp
+//
+//  Created by Aleksandar Filipov on 5/30/22.
+//
+
+import SwiftUI
+
+@main
+struct DocBasedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DocBasedApp/DocBasedApp/TextFile.swift
@@ -0,0 +1,8 @@
+//
+//  TextFile.swift
+//  DocBasedApp
+//
+//  Created by Aleksandar Filipov on 5/30/22.
+//
+
+import Foundation
