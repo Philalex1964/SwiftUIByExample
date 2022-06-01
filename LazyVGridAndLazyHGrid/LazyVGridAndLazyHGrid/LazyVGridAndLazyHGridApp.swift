ab294f7bfae88c86cf806b57b1a736b3e50ed712
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-06-01 14:23:06 +0200

    How to position views in a grid using LazyVGrid and LazyHGrid

---
+++ LazyVGridAndLazyHGrid/LazyVGridAndLazyHGrid/LazyVGridAndLazyHGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyVGridAndLazyHGridApp.swift
+//  LazyVGridAndLazyHGrid
+//
+//  Created by Aleksandar Filipov on 6/1/22.
+//
+
+import SwiftUI
+
+@main
+struct LazyVGridAndLazyHGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
