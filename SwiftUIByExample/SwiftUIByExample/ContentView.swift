69d662d5c3e21ba91dd6b8b559bde863fd39a47d
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-06-01 11:23:58 +0200

    What is the @AppStorage property wrapper?

--- SwiftUIByExample/SwiftUIByExample/ContentView.swift
+++ SwiftUIByExample/SwiftUIByExample/ContentView.swift
@@ -8,26 +8,16 @@
 import SwiftUI
  
 struct ContentView: View {
-    @GestureState var dragAmount = CGSize.zero
+    @AppStorage("username") var username: String = "Anonymous"
 
     var body: some View {
-        Image("Example")
-            .resizable()
-            .scaledToFit()
-            .clipShape(RoundedRectangle(cornerRadius: 16))
-            .overlay(
-                    RoundedRectangle(cornerRadius: 16)
-                        .stroke(.blue, lineWidth: 4)
-            )
-            .frame(width: 200, height: 200)
-            .scaledToFit()
-//            .border(.black, width: 2)
-            .offset(dragAmount)
-            .gesture(
-                DragGesture().updating($dragAmount) { value, state, transaction in
-                    state = value.translation
-                }
-            )
+        VStack {
+            Text("Welcome, \(username)!")
+
+            Button("Log in") {
+                username = "@twostraws"
+            }
+        }
     }
 }
 
