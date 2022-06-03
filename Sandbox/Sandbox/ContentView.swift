5a4966f2431a8693894676acbb69fc708887779b
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-06-03 16:39:16 +0200

    How to adjust text alignment using multilineTextAlignment()

--- Sandbox/Sandbox/ContentView.swift
+++ Sandbox/Sandbox/ContentView.swift
@@ -8,32 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    let alignments: [TextAlignment] = [.leading, .center, .trailing]
+    @State private var alignment = TextAlignment.leading
+    
     var body: some View {
-        Text("This is some longer text that is limited to three lines maximum, so anything more than that will cause the text to clip.")
-            .lineLimit(3)
-            .frame(width: 200)
-        
-        Text("This is some longer text that is limited to three lines maximum, so anything more than that will cause the text to clip.")
-            .lineLimit(1)
-            .truncationMode(.middle)
-        
-        Text("This is an extremely long text string that will never fit even the widest of phones without wrapping")
-            .font(.largeTitle)
-            .frame(width: 300)
-        
-        Text("The best laid plans")
-            .foregroundColor(.red)
-        
-        Text("The best laid plans")
-            .padding()
-            .background(.yellow)
-            .foregroundColor(.white)
-            .font(.headline)
-        
-        Text("This is an extremely long text string that will never fit even the widest of phones without wrapping")
-            .font(.largeTitle)
-            .lineSpacing(50)
-            .frame(width: 300)
+        VStack {
+            Picker("Text alignment", selection: $alignment) {
+                ForEach(alignments, id: \.self) { alignment in
+                    Text(String(describing: alignment))
+                }
+            }
+            
+            Text("This is an extremely long text string that will never fit even the widest of phones without wrapping")
+                .font(.largeTitle)
+                .multilineTextAlignment(alignment)
+                .frame(width: 300)
+        }
     }
 }
 
