484a193db2595739422c06c23a218edddd04c0c7
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-06-03 14:56:05 +0200

    Presenting an alert

--- iDine/iDine/Views/CheckoutView.swift
+++ iDine/iDine/Views/CheckoutView.swift
@@ -14,9 +14,20 @@ struct CheckoutView: View {
     @State private var addLoyaltyDetails = false
     @State private var loyaltyNumber = ""
     @State private var tipAmount = 15
+    @State private var showingPaymentAlert = false
     
     let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
     let tipAmounts = [10, 15, 20, 25, 0]
+    
+    var totalPrice: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+
+        let total = Double(order.total)
+        let tipValue = total / 100 * Double(tipAmount)
+
+        return formatter.string(from: NSNumber(value: total + tipValue)) ?? "$0"
+    }
     
     var body: some View {
         Form {
@@ -49,52 +60,19 @@ struct CheckoutView: View {
             }
             
             Section(header:
-                        Text("TOTAL: $100")
+                        Text("TOTAL: \(totalPrice)")
             ) {
                 Button("Confirm order") {
-                    // place the order
+                    showingPaymentAlert.toggle()
                 }
             }
             //            }
             .navigationTitle("Payment")
             .navigationBarTitleDisplayMode(.inline)
         }
-        //        VStack {
-        //            Section {
-        //                Picker("How do you want to pay?", selection: $paymentType) {
-        //                    ForEach(paymentTypes, id: \.self) {
-        //                        Text($0)
-        //                    }
-        //                }
-        ////                .pickerStyle(.automatic)
-        //
-        //                Toggle("Add iDine loyalty card", isOn: $addLoyaltyDetails.animation())
-        //
-        //                if addLoyaltyDetails {
-        //                    TextField("Enter your iDine ID", text: $loyaltyNumber)
-        //                }
-        //
-        //                Section(header: Text("Add a tip?")) {
-        //                    Picker("Percentage:", selection: $tipAmount) {
-        //                        ForEach(tipAmounts, id: \.self) {
-        //                            Text("\($0)%")
-        //                        }
-        //                    }
-        //                    .pickerStyle(.segmented)
-        //                }
-        //
-        //                Section(header:
-        //                    Text("TOTAL: $100")
-        //                ) {
-        //                    Button("Confirm order") {
-        //                        // place the order
-        //                    }
-        //                }
-        //
-        //            }
-        //        }
-        //        .navigationTitle("Payment")
-        //        .navigationBarTitleDisplayMode(.inline)
+        .alert(isPresented: $showingPaymentAlert) {
+            Alert(title: Text("Order confirmed"), message: Text("Your total was \(totalPrice) – thank you!"), dismissButton: .default(Text("Ok")))
+        }
     }
 }
 
