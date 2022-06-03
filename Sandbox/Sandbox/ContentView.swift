6738f58fd33e6f95d55a4798c5c4e4cde31a9ea5
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-06-03 16:55:32 +0200

    How to format text inside text views

--- Sandbox/Sandbox/ContentView.swift
+++ Sandbox/Sandbox/ContentView.swift
@@ -8,25 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
-    let alignments: [TextAlignment] = [.leading, .center, .trailing]
-    @State private var alignment = TextAlignment.leading
+    @State private var ingredients = [String]()
+    @State private var rolls = [Int]()
+    
+    let length = Measurement(value: 225, unit: UnitLength.centimeters)
+    
+//    static let taskDateFormat: DateFormatter = {
+//            let formatter = DateFormatter()
+//            formatter.dateStyle = .long
+//            return formatter
+//        }()
+
+    let dueDate = Date()
     
     var body: some View {
         VStack {
-            Picker("Text alignment", selection: $alignment) {
-                ForEach(alignments, id: \.self) { alignment in
-                    Text(String(describing: alignment))
+            Text(ingredients, format: .list(type: .and))
+            
+            Button("Add Ingredient") {
+                let possibles = ["Egg", "Sausage", "Bacon", "Spam"]
+                
+                if let newIngredient = possibles.randomElement() {
+                    ingredients.append(newIngredient)
                 }
             }
-            
-            Text("This is an extremely long text string that will never fit even the widest of phones without wrapping")
-                .font(.largeTitle)
-                .multilineTextAlignment(alignment)
-                .frame(width: 300)
         }
+        
+        VStack {
+            Text(rolls, format: .list(memberStyle: .number, type: .and))
+            
+            Button("Roll Dice") {
+                let result = Int.random(in: 1...6)
+                rolls.append(result)
+            }
+        }
+        
+        Text(length, format: .measurement(width: .wide))
+        
+        Text(72.3, format: .currency(code: "CAD"))
+        
+//        Text("Task due date: \(dueDate, formatter: Self.taskDateFormat)")
+        Text("Task due date: \(dueDate, format: .dateTime)")
+
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
