e65158fca985c7c899e938abac8b0fc08584f7a4
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-06-03 10:59:39 +0200

    set up project with assets

---
+++ iDine/iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Aleksandar Filipov on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
