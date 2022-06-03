66c0b393723a5a8f17eee48db47d1dbca1e26051
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-06-03 09:23:23 +0200

    Sandbox started

---
+++ Sandbox/Sandbox/SandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandboxApp.swift
+//  Sandbox
+//
+//  Created by Aleksandar Filipov on 6/1/22.
+//
+
+import SwiftUI
+
+@main
+struct SandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
