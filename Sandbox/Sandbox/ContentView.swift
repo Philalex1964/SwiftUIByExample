0ad919233d82cb636d169662448157d343e0c95d
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-06-06 11:40:48 +0200

    When should you use ContainerRelativeShape?

--- Sandbox/Sandbox/ContentView.swift
+++ Sandbox/Sandbox/ContentView.swift
@@ -7,37 +7,19 @@
 
 import SwiftUI
 
-
-//struct ContentView: View {
-//
-//    var body: some View {
-//        Circle()
-//            .trim(from: 0, to: 0.5)
-//            .frame(width: 200, height: 200)
-////            .rotationEffect(.degrees(180))
-//    }
-//}
-
 struct ContentView: View {
-    @State private var completionAmount = 0.0
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-
     var body: some View {
-        Circle()
-            .trim(from: 0, to: completionAmount)
-//            .fill(.blue)
-            .stroke(.red, lineWidth: 20)
-            .frame(width: 200, height: 200)
-            .rotationEffect(.degrees(-90))
-            .onReceive(timer) { _ in
-                withAnimation {
-                    if completionAmount == 1 {
-                        completionAmount = 0
-                    } else {
-                        completionAmount += 0.25
-                    }
-                }
-            }
+        ZStack {
+            ContainerRelativeShape()
+                .inset(by: 10)
+                .fill(.blue)
+
+            Text("Hello!")
+                .font(.title)
+        }
+        .frame(width: 200, height: 200)
+        .background(.red)
+        .clipShape(RoundedRectangle(cornerRadius: 15))
     }
 }
 
