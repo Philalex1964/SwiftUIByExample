9d52ecffb5740adb95ac382f85e88b76141ab6bb
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-06-06 12:19:32 +0200

    How to get custom colors and transparency with SF Symbols

--- Sandbox/Sandbox/ContentView.swift
+++ Sandbox/Sandbox/ContentView.swift
@@ -10,20 +10,36 @@ import SwiftUI
 struct ContentView: View {
 
     var body: some View {
-        AsyncImage(url: URL(string: "https://www.hackingwithswift.com/img/paul.png"))
-
-        AsyncImage(url: URL(string: "https://www.hackingwithswift.com/img/paul100.png"))
-
-        AsyncImage(url: URL(string: "https://www.hackingwithswift.com/img/paul-2.png")) { image in
-            image.resizable()
-        } placeholder: {
-            Color.red
-        }
-        .frame(width: 128, height: 128)
-        .clipShape(RoundedRectangle(cornerRadius: 25))
-
-        AsyncImage(url: URL(string: "https://www.hackingwithswift.com/img/paul@2x.png"), scale: 2)
+        Image(systemName: "theatermasks")
+            .symbolRenderingMode(.hierarchical)
+            .font(.system(size: 144))
+        
+        Image(systemName: "theatermasks")
+            .symbolRenderingMode(.hierarchical)
+            .foregroundColor(.blue)
+            .font(.system(size: 144))
+        
+        Image(systemName: "shareplay")
+            .symbolRenderingMode(.palette)
+            .foregroundStyle(.blue, .black)
+            .font(.system(size: 144))
+        
+        Image(systemName: "person.3.sequence.fill")
+            .symbolRenderingMode(.palette)
+            .foregroundStyle(.blue, .green, .red)
+            .font(.system(size: 144))
+        
+        Image(systemName: "person.3.sequence.fill")
+            .symbolRenderingMode(.palette)
+            .foregroundStyle(
+                .linearGradient(colors: [.red, .black], startPoint: .top, endPoint: .bottomTrailing),
+                .linearGradient(colors: [.green, .black], startPoint: .top, endPoint: .bottomTrailing),
+                .linearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottomTrailing)
+            )
+            .font(.system(size: 144))
     }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
