3c2d83d9bed06843a093cdaf6d8a0e86fc089c39
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-06-06 09:52:53 +0200

    How to show text and an icon side by side using Label

--- Sandbox/Sandbox/ContentView.swift
+++ Sandbox/Sandbox/ContentView.swift
@@ -8,13 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var name = "Paul"
 
     var body: some View {
-        TextField("Shout your name at me", text: $name)
-            .textFieldStyle(.roundedBorder)
-            .textCase(.uppercase)
-            .padding(.horizontal)
+        Label("Your account", systemImage: "folder.circle")
+            .font(.title)
+        
+        Label("Welcome to the app", image: "Star")
+        
+        VStack {
+            Label("Text Only", systemImage: "heart")
+                .font(.title)
+                .labelStyle(.titleOnly)
+
+            Label("Icon Only", systemImage: "star")
+                .font(.title)
+                .labelStyle(.iconOnly)
+
+            Label("Both", systemImage: "paperplane")
+                .font(.title)
+                .labelStyle(.titleAndIcon)
+        }
+        
+        Label {
+            Text("Paul Hudson")
+                .foregroundColor(.primary)
+                .font(.largeTitle)
+                .padding()
+                .background(.gray.opacity(0.2))
+                .clipShape(Capsule())
+        } icon: {
+            RoundedRectangle(cornerRadius: 10)
+                .fill(.blue)
+                .frame(width: 64, height: 64)
+        }
     }
 }
 struct ContentView_Previews: PreviewProvider {
