07e5db4fc58a42a93bbd23fa865cdd76ce9227c5
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-06-06 15:54:04 +0200

    How to inset the safe area with custom content

--- Sandbox/Sandbox/ContentView.swift
+++ Sandbox/Sandbox/ContentView.swift
@@ -9,26 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            Image(systemName: "clock.fill")
-            Text("Set the time")
+        NavigationView {
+            List(0..<100) { i in
+                Text("Row \(i)")
+            }
+            .navigationTitle("Select a row")
+            .safeAreaInset(edge: .bottom) {
+                Text("Outside Safe Area")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(.indigo)
+            }
         }
-        .font(.largeTitle.bold())
-        .foregroundStyle(.quaternary)
         
-        HStack {
-            Image(systemName: "clock.fill")
-            Text("Set the time")
+        NavigationView {
+            List(0..<100) { i in
+                Text("Row \(i)")
+            }
+            .navigationTitle("Select a row")
+            .safeAreaInset(edge: .bottom, alignment: .trailing) {
+                Button {
+                    print("Show help")
+                } label: {
+                    Image(systemName: "info.circle.fill")
+                        .font(.largeTitle)
+                        .symbolRenderingMode(.multicolor)
+                        .padding(.trailing)
+                }
+                .accessibilityLabel("Show help")
+            }
         }
-        .font(.largeTitle.bold())
-        .foregroundStyle(
-            .linearGradient(
-                colors: [.red, .black],
-                startPoint: .top,
-                endPoint: .bottom
-            )
-        )
-
     }
 }
 
