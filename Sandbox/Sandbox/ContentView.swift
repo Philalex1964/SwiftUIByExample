2b3308f8709947d828a6d8cd49abd290b7561431
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-06-06 09:46:39 +0200

    How to make TextField uppercase or lowercase using textCase()

--- Sandbox/Sandbox/ContentView.swift
+++ Sandbox/Sandbox/ContentView.swift
@@ -8,29 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var amount = 50.0
+    @State private var name = "Paul"
 
     var body: some View {
-        Text(Date()...Date().addingTimeInterval(600))
-
-        VStack {
-            // show just the date
-            Text(Date().addingTimeInterval(600), style: .date)
-
-            // show just the time
-            Text(Date().addingTimeInterval(0), style: .time)
-
-            // show the relative distance from now, automatically updating
-            Text(Date().addingTimeInterval(600), style: .relative)
-
-            // make a timer style, automatically updating
-            Text(Date().addingTimeInterval(600), style: .timer)
-        }
-        
-//        Text(Date())
+        TextField("Shout your name at me", text: $name)
+            .textFieldStyle(.roundedBorder)
+            .textCase(.uppercase)
+            .padding(.horizontal)
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
