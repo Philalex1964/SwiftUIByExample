e215cd005e511f3dc0b3d8fd9aea8c14bc67be6b
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-06-06 11:16:33 +0200

    How to display solid shapes

--- Sandbox/Sandbox/ContentView.swift
+++ Sandbox/Sandbox/ContentView.swift
@@ -10,30 +10,21 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        Text("Hacking with Swift")
-            .font(.system(size: 48))
-            .padding(50)
-            .background(
-                Image("Example")
-                    .resizable()
-            )
+        Rectangle()
+            .fill(.red)
+            .frame(width: 200, height: 200)
         
-        Text("Hacking with Swift")
-            .font(.largeTitle)
-            .padding()
-            .background(Circle()
-                .fill(.red)
-                .frame(width: 50, height: 50))
-
-        Text("Hacking with Swift")
-            .font(.largeTitle)
-            .padding()
-            .background(
-                Circle()
-                    .fill(.red)
-                    .frame(width: 100, height: 100)
-            )
-            .clipped()
+        Circle()
+            .fill(.blue)
+            .frame(width: 100, height: 100)
+        
+        RoundedRectangle(cornerRadius: 25)
+            .fill(.green)
+            .frame(width: 150, height: 100)
+        
+        Capsule()
+            .fill(.green)
+            .frame(width: 150, height: 100)
     }
 }
 struct ContentView_Previews: PreviewProvider {
