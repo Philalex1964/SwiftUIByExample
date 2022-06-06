38f88c13bb13654aa2beec0b5075ffa7378b4df2
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-06-06 09:34:41 +0200

    How to add spacing between letters in text

--- Sandbox/Sandbox/ContentView.swift
+++ Sandbox/Sandbox/ContentView.swift
@@ -8,50 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var ingredients = [String]()
-    @State private var rolls = [Int]()
-    
-    let length = Measurement(value: 225, unit: UnitLength.centimeters)
-    
-//    static let taskDateFormat: DateFormatter = {
-//            let formatter = DateFormatter()
-//            formatter.dateStyle = .long
-//            return formatter
-//        }()
+    @State private var amount = 50.0
 
-    let dueDate = Date()
-    
     var body: some View {
         VStack {
-            Text(ingredients, format: .list(type: .and))
-            
-            Button("Add Ingredient") {
-                let possibles = ["Egg", "Sausage", "Bacon", "Spam"]
-                
-                if let newIngredient = possibles.randomElement() {
-                    ingredients.append(newIngredient)
-                }
-            }
-        }
-        
-        VStack {
-            Text(rolls, format: .list(memberStyle: .number, type: .and))
-            
-            Button("Roll Dice") {
-                let result = Int.random(in: 1...6)
-                rolls.append(result)
-            }
-        }
-        
-        Text(length, format: .measurement(width: .wide))
-        
-        Text(72.3, format: .currency(code: "CAD"))
-        
-//        Text("Task due date: \(dueDate, formatter: Self.taskDateFormat)")
-        Text("Task due date: \(dueDate, format: .dateTime)")
+            Text("ffi")
+                .font(.custom("AmericanTypewriter", size: 72))
+                .kerning(amount)
+            Text("ffi")
+                .font(.custom("AmericanTypewriter", size: 72))
+                .tracking(amount)
 
+            Slider(value: $amount, in: 0...100) {
+                Text("Adjust the amount of spacing")
+            }
+        }
+        
+        Text("Hello World")
+            .tracking(20)
     }
 }
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
