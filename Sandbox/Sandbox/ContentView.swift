e1d02e9622ec6d7bda6cc64db3df37ecb4bddb70
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-06-06 16:53:53 +0200

    How to create 3D effects like Cover Flow using ScrollView and GeometryReader

--- Sandbox/Sandbox/ContentView.swift
+++ Sandbox/Sandbox/ContentView.swift
@@ -7,50 +7,55 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    let colors: [Color] = [.red, .green, .blue]
-    
-    var body: some View {
-        ScrollView {
-            ScrollViewReader { value in
-                Button("Jump to #90") {
-                    value.scrollTo(90)
-                }
-                .padding()
-                
-                ForEach(0..<100) { i in
-                    Text("Example \(i)")
-                        .font(.title)
-                        .frame(width: 200, height: 200)
-                        .background(colors[i % colors.count])
-                        .id(i)
-                }
-            }
-        }
-        .frame(height: 350)
-        
-        ScrollView {
-                    ScrollViewReader { value in
-                        Button("Jump to #8") {
-                            value.scrollTo(8, anchor: .top)
-                        }
-                        .padding()
+//struct ContentView: View {
+//    let colors: [Color] = [.red, .green, .blue]
+//
+//    var body: some View {
+//        ScrollView(.horizontal, showsIndicators: false) {
+//            HStack(spacing: 0) {
+//                ForEach(1..<20) { num in
+//                    VStack {
+//                        GeometryReader { geo in
+//                            Text("Number \(num)")
+//                                .font(.largeTitle)
+//                                .padding()
+//                                .background(.red)
+//                                .rotation3DEffect(.degrees(-Double(geo.frame(in: .global).minX) / 8), axis: (x: 0, y: 1, z: 0))
+//                                .frame(width: 200, height: 200)
+//                        }
+//                        .frame(width: 200, height: 200)
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
 
-                        ForEach(0..<100) { i in
-                            Text("Example \(i)")
-                                .font(.title)
-                                .frame(width: 200, height: 200)
-                                .background(colors[i % colors.count])
-                                .id(i)
+struct ContentView: View {
+    @State var dragAmount = CGSize.zero
+
+    var body: some View {
+        VStack {
+            Rectangle()
+                .fill(LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing))
+                .frame(width: 200, height: 150)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .rotation3DEffect(.degrees(-Double(dragAmount.width) / 20), axis: (x: 0, y: 1, z: 0))
+                .rotation3DEffect(.degrees(Double(dragAmount.height / 20)), axis: (x: 1, y: 0, z: 0))
+                .offset(dragAmount)
+                .gesture(
+                    DragGesture()
+                        .onChanged { dragAmount = $0.translation }
+                        .onEnded { _ in
+                            withAnimation(.spring()) {
+                                dragAmount = .zero
+                            }
                         }
-                    }
-                }
-                .frame(height: 350)
+                )
+        }
+        .frame(width: 400, height: 400)
     }
 }
-
-//If you call scrollTo() inside withAnimation() the movement will be animated.
-//Tip: scrollTo() works great with lists too!
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
