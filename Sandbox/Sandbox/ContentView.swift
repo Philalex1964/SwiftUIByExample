8ab94c359510fef6e15089b17cdee56623ec5932
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-06-06 11:34:44 +0200

    How to draw part of a solid shape using trim()

--- Sandbox/Sandbox/ContentView.swift
+++ Sandbox/Sandbox/ContentView.swift
@@ -8,44 +8,41 @@
 import SwiftUI
 
 
+//struct ContentView: View {
+//
+//    var body: some View {
+//        Circle()
+//            .trim(from: 0, to: 0.5)
+//            .frame(width: 200, height: 200)
+////            .rotationEffect(.degrees(180))
+//    }
+//}
+
 struct ContentView: View {
-    
+    @State private var completionAmount = 0.0
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+
     var body: some View {
         Circle()
-            .strokeBorder(.black, lineWidth: 20)
-            .background(Circle().fill(.blue))
-            .frame(width: 150, height: 150)
-        
-        ZStack {
-            Circle()
-                .fill(.red)
-
-            Circle()
-                .strokeBorder(.black, lineWidth: 20)
-        }
-        .frame(width: 150, height: 150)
+            .trim(from: 0, to: completionAmount)
+//            .fill(.blue)
+            .stroke(.red, lineWidth: 20)
+            .frame(width: 200, height: 200)
+            .rotationEffect(.degrees(-90))
+            .onReceive(timer) { _ in
+                withAnimation {
+                    if completionAmount == 1 {
+                        completionAmount = 0
+                    } else {
+                        completionAmount += 0.25
+                    }
+                }
+            }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-
-// Errors?!
-extension Shape {
-    func fill<Fill: ShapeStyle, Stroke: ShapeStyle>(_ fillStyle: Fill, strokeBorder strokeStyle: Stroke, lineWidth: Double = 1) -> some View {
-        self
-            .stroke(strokeStyle, lineWidth: lineWidth)
-            .background(self.fill(fillStyle))
-    }
-}
-
-extension InsettableShape {
-    func fill<Fill: ShapeStyle, Stroke: ShapeStyle>(_ fillStyle: Fill, strokeBorder strokeStyle: Stroke, lineWidth: Double = 1) -> some View {
-        self
-            .strokeBorder(strokeStyle, lineWidth: lineWidth)
-            .background(self.fill(fillStyle))
     }
 }
