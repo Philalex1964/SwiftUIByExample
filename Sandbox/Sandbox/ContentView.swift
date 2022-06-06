6219b4489e029c47df5470fbf297120b3f915051
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-06-06 14:08:48 +0200

    How to return different view types

--- Sandbox/Sandbox/ContentView.swift
+++ Sandbox/Sandbox/ContentView.swift
@@ -7,13 +7,100 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    
+// first option is to wrap your output in a group, so that no matter whether you send back an image or a text view they both go back in a group
+//struct ContentView: View {
+//    var tossResult: some View {
+//        Group {
+//            if Bool.random() {
+//                Image("paul")
+//                    .resizable()
+//                    .scaledToFit()
+//            } else {
+//                Text("Better luck next time")
+//                    .font(.title)
+//            }
+//        }
+//        .frame(width: 400, height: 300)
+//    }
+//
+//    var body: some View {
+//        VStack {
+//            Text("Coin Flip")
+//                .font(.largeTitle)
+//
+//            tossResult
+//        }
+//    }
+//}
+
+// The second is to use a type-erased wrapper called AnyView that we can return:
+//struct ContentView: View {
+//    var tossResult: some View {
+//        if Bool.random() {
+//            return AnyView(Image("Example").resizable().scaledToFit())
+//        } else {
+//            return AnyView(Text("Better luck next time").font(.title))
+//        }
+//    }
+//
+//    var body: some View {
+//        VStack {
+//            Text("Coin Flip")
+//                .font(.largeTitle)
+//
+//            tossResult
+//                .frame(width: 400, height: 300)
+//        }
+//    }
+//}
+
+// A third option is to apply the @ViewBuilder attribute yourself to any properties that need it, like this:
+//struct ContentView: View {
+//    @ViewBuilder var tossResult: some View {
+//        if Bool.random() {
+//            Image("Example")
+//                .resizable()
+//                .scaledToFit()
+//        } else {
+//            Text("Better luck next time")
+//                .font(.title)
+//        }
+//    }
+//
+//    var body: some View {
+//        VStack {
+//            Text("Coin Flip")
+//                .font(.largeTitle)
+//
+//            tossResult
+//                .frame(width: 400, height: 300)
+//        }
+//    }
+//}
+
+// The fourth solution, and the one that works best the majority of the time, is to break up your views into smaller views, then combine them together as needed:
+struct TossResult: View {
     var body: some View {
-        Text("Hello World")
-            .frame(minWidth: 100, maxWidth: .infinity, minHeight: 100, maxHeight: .infinity)
-            .background(.red)
-            .ignoresSafeArea()
+        if Bool.random() {
+            Image("Example")
+                .resizable()
+                .scaledToFit()
+        } else {
+            Text("Better luck next time")
+                .font(.title)
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Coin Flip")
+                .font(.largeTitle)
+
+            TossResult()
+                .frame(width: 400, height: 300)
+        }
     }
 }
 
