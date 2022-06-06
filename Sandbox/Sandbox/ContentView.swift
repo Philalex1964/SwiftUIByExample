dc50ef55fdb382fe1c4f3558fcc27ab7c6d874bd
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-06-06 11:26:57 +0200

    How to fill and stroke shapes at the same time

--- Sandbox/Sandbox/ContentView.swift
+++ Sandbox/Sandbox/ContentView.swift
@@ -7,28 +7,45 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     
     var body: some View {
-        Rectangle()
-            .fill(.red)
-            .frame(width: 200, height: 200)
-        
         Circle()
-            .fill(.blue)
-            .frame(width: 100, height: 100)
+            .strokeBorder(.black, lineWidth: 20)
+            .background(Circle().fill(.blue))
+            .frame(width: 150, height: 150)
         
-        RoundedRectangle(cornerRadius: 25)
-            .fill(.green)
-            .frame(width: 150, height: 100)
-        
-        Capsule()
-            .fill(.green)
-            .frame(width: 150, height: 100)
+        ZStack {
+            Circle()
+                .fill(.red)
+
+            Circle()
+                .strokeBorder(.black, lineWidth: 20)
+        }
+        .frame(width: 150, height: 150)
     }
 }
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+// Errors?!
+extension Shape {
+    func fill<Fill: ShapeStyle, Stroke: ShapeStyle>(_ fillStyle: Fill, strokeBorder strokeStyle: Stroke, lineWidth: Double = 1) -> some View {
+        self
+            .stroke(strokeStyle, lineWidth: lineWidth)
+            .background(self.fill(fillStyle))
+    }
+}
+
+extension InsettableShape {
+    func fill<Fill: ShapeStyle, Stroke: ShapeStyle>(_ fillStyle: Fill, strokeBorder strokeStyle: Stroke, lineWidth: Double = 1) -> some View {
+        self
+            .strokeBorder(strokeStyle, lineWidth: lineWidth)
+            .background(self.fill(fillStyle))
     }
 }
