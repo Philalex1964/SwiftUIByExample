39397f2fddaefcdf67b1a269e5e3a6f903dc43bd
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-06-06 12:00:43 +0200

    How to integrate SpriteKit using SpriteView

--- Sandbox/Sandbox/ContentView.swift
+++ Sandbox/Sandbox/ContentView.swift
@@ -6,25 +6,38 @@
 //
 
 import SwiftUI
-import AVKit
+import SpriteKit
 
+// A simple game scene with falling boxes
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let box = SKSpriteNode(color: .red, size: CGSize(width: 50, height: 50))
+        box.position = location
+        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
+        addChild(box)
+    }
+}
+
+// A sample SwiftUI creating a GameScene and sizing it
+// at 300x400 points
 struct ContentView: View {
+    var scene: SKScene {
+        let scene = GameScene()
+        scene.size = CGSize(width: 300, height: 400)
+        scene.scaleMode = .fill
+        return scene
+    }
+
     var body: some View {
-        VideoPlayer(player: AVPlayer(url:  Bundle.main.url(forResource: "example", withExtension: "mov")!))
-            .frame(height: 400)
-        
-        VideoPlayer(player: AVPlayer(url:  URL(string: "https://bit.ly/swswift")!))
-            .frame(height: 400)
-        
-        VideoPlayer(player: AVPlayer(url:  URL(string: "https://bit.ly/swswift")!)) {
-            VStack {
-                Text("Watermark")
-                    .foregroundColor(.black)
-                    .background(.white.opacity(0.7))
-                Spacer()
-            }
-            .frame(width: 400, height: 300)
-        }
+        SpriteView(scene: scene)
+            .frame(width: 300, height: 400)
+            .ignoresSafeArea()
     }
 }
 
