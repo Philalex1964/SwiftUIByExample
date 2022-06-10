6d311f7faad49bc886dd70c879e5685af68fb9b3
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-06-10 16:06:10 +0200

    How to dismiss the keyboard for a TextField before iOS15

--- Sandbox/Sandbox/ContentView.swift
+++ Sandbox/Sandbox/ContentView.swift
@@ -7,73 +7,30 @@
 
 import SwiftUI
 
-//struct ContentView: View {
-//    @State private var name = ""
-//    @FocusState private var nameIsFocused: Bool
-//
-//    var body: some View {
-//        VStack {
-//            TextField("Enter your name", text: $name)
-//                .focused($nameIsFocused)
-//
-//            Button("Submit") {
-//                nameIsFocused = false
-//            }
-//        }
-//    }
-//}
-
 struct ContentView: View {
-    enum Field {
-        case firstName
-        case lastName
-        case emailAddress
-    }
-
-    @State private var firstName = ""
-    @State private var lastName = ""
-    @State private var emailAddress = ""
-    @FocusState private var focusedField: Field?
-
+    @State private var tipAmount = ""
+    
     var body: some View {
         VStack {
-            TextField("Enter your first name", text: $firstName)
-                .focused($focusedField, equals: .firstName)
-                .textContentType(.givenName)
-                .submitLabel(.next)
+            TextField("Tip Amount ", text: $tipAmount)
+                .textFieldStyle(.roundedBorder)
+                .keyboardType(.decimalPad)
             
-            if firstName.isEmpty {
-                TextField("Enter your last name", text: $lastName)
-                    .disabled(true)
-            } else {
-                TextField("Enter your last name", text: $lastName)
-                    .focused($focusedField, equals: .lastName)
-                    .textContentType(.familyName)
-                    .submitLabel(.next)
-            }
-
-            if firstName.isEmpty || lastName.isEmpty {
-                TextField("Enter your email address", text: $emailAddress)
-                    .disabled(true)
-            } else {
-                TextField("Enter your email address", text: $emailAddress)
-                    .focused($focusedField, equals: .emailAddress)
-                    .textContentType(.emailAddress)
-                    .submitLabel(.join)
-            }
-        }
-        .onSubmit {
-            switch focusedField {
-            case .firstName:
-                focusedField = .lastName
-            case .lastName:
-                focusedField = .emailAddress
-            default:
-                print("Creating account…")
+            Button("Submit") {
+                print("Tip: \(tipAmount)")
+                hideKeyboard()
             }
         }
     }
 }
+
+#if canImport(UIKit)
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+#endif
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
