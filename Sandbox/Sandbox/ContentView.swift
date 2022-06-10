7b052a0698d33d08c234b1f3b33a121eaf052057
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-06-10 15:58:32 +0200

    How to dismiss the keyboard for a TextField iOS15+

--- Sandbox/Sandbox/ContentView.swift
+++ Sandbox/Sandbox/ContentView.swift
@@ -7,15 +7,70 @@
 
 import SwiftUI
 
+//struct ContentView: View {
+//    @State private var name = ""
+//    @FocusState private var nameIsFocused: Bool
+//
+//    var body: some View {
+//        VStack {
+//            TextField("Enter your name", text: $name)
+//                .focused($nameIsFocused)
+//
+//            Button("Submit") {
+//                nameIsFocused = false
+//            }
+//        }
+//    }
+//}
+
 struct ContentView: View {
-    @State private var name: String = ""
+    enum Field {
+        case firstName
+        case lastName
+        case emailAddress
+    }
+
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var emailAddress = ""
+    @FocusState private var focusedField: Field?
 
     var body: some View {
-        VStack(alignment: .leading) {
-            TextField("Enter your name", text: $name)
-                .textFieldStyle(.roundedBorder)
-                .autocorrectionDisabled()
-            Text("Hello, \(name)!")
+        VStack {
+            TextField("Enter your first name", text: $firstName)
+                .focused($focusedField, equals: .firstName)
+                .textContentType(.givenName)
+                .submitLabel(.next)
+            
+            if firstName.isEmpty {
+                TextField("Enter your last name", text: $lastName)
+                    .disabled(true)
+            } else {
+                TextField("Enter your last name", text: $lastName)
+                    .focused($focusedField, equals: .lastName)
+                    .textContentType(.familyName)
+                    .submitLabel(.next)
+            }
+
+            if firstName.isEmpty || lastName.isEmpty {
+                TextField("Enter your email address", text: $emailAddress)
+                    .disabled(true)
+            } else {
+                TextField("Enter your email address", text: $emailAddress)
+                    .focused($focusedField, equals: .emailAddress)
+                    .textContentType(.emailAddress)
+                    .submitLabel(.join)
+            }
+        }
+        .onSubmit {
+            switch focusedField {
+            case .firstName:
+                focusedField = .lastName
+            case .lastName:
+                focusedField = .emailAddress
+            default:
+                print("Creating account…")
+            }
         }
     }
 }
