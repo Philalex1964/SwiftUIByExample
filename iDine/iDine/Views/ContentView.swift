3eb154aa09b38d9e5b1e56ff85e2ce7aec7e5021
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-06-10 13:41:45 +0200

    added HStack

--- iDine/iDine/Views/ContentView.swift
+++ iDine/iDine/Views/ContentView.swift
@@ -11,20 +11,24 @@ struct ContentView: View {
     let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
     
     var body: some View {
-        NavigationView {
-            List{
-                ForEach(menu) { section in
-                    Section(header: Text(section.name)) {
-                        ForEach(section.items) { item in
-                            NavigationLink (destination: ItemDetail(item: item)) {
-                                ItemRow(item: item)
+        HStack {
+            NavigationView {
+                List{
+                    ForEach(menu) { section in
+                        Section(header: Text(section.name)) {
+                            ForEach(section.items) { item in
+                                NavigationLink (destination: ItemDetail(item: item)) {
+                                    ItemRow(item: item)
+                                }
                             }
                         }
                     }
+                    
+                    
                 }
+                .navigationTitle("Menu")
+                .listStyle(.grouped)
             }
-            .navigationTitle("Menu")
-            .listStyle(.grouped)
         }
     }
 }
