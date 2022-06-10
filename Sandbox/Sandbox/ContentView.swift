456fbd24ee17e7ce80880f764a5ecb00a671c542
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-06-10 14:59:01 +0200

    reset

--- Sandbox/Sandbox/ContentView.swift
+++ Sandbox/Sandbox/ContentView.swift
@@ -7,48 +7,16 @@
 
 import SwiftUI
 
-//struct ContentView: View {
-//    var body: some View {
-//        ScrollView {
-//            LazyVStack {
-//                ForEach(1...100, id: \.self) { value in
-//                    Text("Row \(value)")
-//                }
-//            }
-//        }
-//        .frame(height: 300)
-//    }
-//}
-
-struct SampleRow: View {
-    let id: Int
-
-    var body: some View {
-        Text("Row \(id)")
-    }
-
-    init(id: Int) {
-        print("Loading row \(id)")
-        self.id = id
-    }
-}
-
 struct ContentView: View {
     var body: some View {
-        ScrollView {
-            LazyVStack {
-                ForEach(1...100, id: \.self, content: SampleRow.init)
-            }
-        }
-        .frame(height: 300)
+        
+        Text("05/08/2022")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            ContentView()
-                .previewInterfaceOrientation(.portrait)
             ContentView()
         }
     }
