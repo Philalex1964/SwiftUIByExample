66a3208367828db44931d97d777a4fcda4941d10
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-06-13 08:38:02 +0200

    reset

--- Sandbox/Sandbox/ContentView.swift
+++ Sandbox/Sandbox/ContentView.swift
@@ -7,33 +7,6 @@
 
 import SwiftUI
 
-//struct ContentView: View {
-//    @State private var showGreeting = true
-//
-//    var body: some View {
-//        VStack {
-//            Toggle("Show welcome message", isOn: $showGreeting)
-////                .toggleStyle(SwitchToggleStyle(tint: .red))
-//                .tint(.yellow)
-//
-//            if showGreeting {
-//                Text("Hello World!")
-//            }
-//        }
-//    }
-//}
-
-// button style iOS15+
-struct ContentView: View {
-    @State private var isOn = false
-
-    var body: some View {
-        Toggle("Filter", isOn: $isOn)
-            .toggleStyle(.button)
-            .tint(.mint)
-    }
-}
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
